dd010fd46198b96aa2f2c5fa487e63cd92577f4f
Abhay Kumar <41410087+abhay-k@users.noreply.github.com>
2026-03-01 14:55:20 -0500

    Create Create forecast.py

---
+++ Create forecast.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from prophet import Prophet
+
+def prophet_forecast(data):
+
+    df = pd.DataFrame(data)
+    df.columns = ["ds","y"]
+
+    model = Prophet()
+    model.fit(df)
+
+    future = model.make_future_dataframe(periods=30)
+
+    forecast = model.predict(future)
+
+    return forecast[['ds','yhat']].tail(30).to_dict()
