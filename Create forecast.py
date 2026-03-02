303d77bacb6f10d04d8f1824107a135bb5125bd0
Abhay Kumar <41410087+abhay-k@users.noreply.github.com>
2026-03-02 08:53:22 -0500

    Update Create forecast.py
    Sales data based

--- Create forecast.py
+++ Create forecast.py
@@ -1,16 +1,12 @@
 import pandas as pd
 from prophet import Prophet
 
-def prophet_forecast(data):
-
-    df = pd.DataFrame(data)
-    df.columns = ["ds","y"]
+def prophet_forecast(df):
 
     model = Prophet()
     model.fit(df)
 
     future = model.make_future_dataframe(periods=30)
-
     forecast = model.predict(future)
 
-    return forecast[['ds','yhat']].tail(30).to_dict()
+    return forecast[['ds','yhat']].tail(30).to_dict(orient="records")
