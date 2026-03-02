1333fd3f24da964cc198d354bbfa2714a47c863e
Abhay Kumar <41410087+abhay-k@users.noreply.github.com>
2026-03-02 09:02:10 -0500

    Update Create prediction.py
    not required

--- Create prediction.py
+++ Create prediction.py
@@ -1,16 +1,1 @@
-from sklearn.linear_model import LogisticRegression
-import pandas as pd
 
-def predict_conversion(data):
-
-    df = pd.DataFrame(data)
-
-    X = df[['visits','time_spent','pages']]
-    y = df['converted']
-
-    model = LogisticRegression()
-    model.fit(X,y)
-
-    prediction = model.predict(X)
-
-    return prediction.tolist()
