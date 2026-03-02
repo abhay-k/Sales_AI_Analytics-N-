59733ad41ad6d02d7fb86e19561c0de1838a5eab
Abhay Kumar <41410087+abhay-k@users.noreply.github.com>
2026-03-02 08:54:33 -0500

    Update report.py
    Sales data based

--- report.py
+++ report.py
@@ -1,20 +1,16 @@
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Image
 from reportlab.lib.styles import getSampleStyleSheet
 
-def generate_report(chart, insight):
+def generate_report(insight, region_chart, customer_chart):
 
     styles = getSampleStyleSheet()
-
     report = SimpleDocTemplate("sales_report.pdf")
-
     content = []
 
     content.append(Paragraph("Sales Analytics Report", styles["Title"]))
-
     content.append(Paragraph(insight, styles["Normal"]))
-
-    content.append(Image(chart))
+    content.append(Image(region_chart))
+    content.append(Image(customer_chart))
 
     report.build(content)
-
     return "sales_report.pdf"
