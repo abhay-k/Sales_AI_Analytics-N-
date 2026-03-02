10a2a1782889f3b3274a39a79cfac2581c14de7d
Abhay Kumar <41410087+abhay-k@users.noreply.github.com>
2026-03-02 18:47:17 -0500

    Update forecasting.py
    Date added

--- forecasting.py
+++ forecasting.py
@@ -2,11 +2,13 @@ import pandas as pd
 from prophet import Prophet
 
 def prophet_forecast(df):
+    # Prophet requires only ds (date) and y (value)
+    ts = df[["ds", "y"]]
 
     model = Prophet()
-    model.fit(df)
+    model.fit(ts)
 
-    future = model.make_future_dataframe(periods=30)
+    future = model.make_future_dataframe(periods=90)
     forecast = model.predict(future)
 
-    return forecast[['ds','yhat']].tail(30).to_dict(orient="records")
+    return forecast[["ds", "yhat"]].tail(30).to_dict(orient="records")
